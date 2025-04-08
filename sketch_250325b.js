5e078f7af97ea7a03e7893c789af7bdffbcf0b45
NickWhite-Program <nicholas.white@sjsu.edu>
2025-04-08 09:52:40 -0700

    Add files via upload

---
+++ sketch_250325b.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(1980, 1080);
+}
+
+function draw() {
+background("pink");
+fill("red");
+rect(720, 100, 100, 150);
+fill("white");
+ellipse(540, 100, 300, 100);
+ellipse(1000, 100, 300, 100);
+fill("red");
+ellipse(540, 300, 300, 100);
+ellipse(1000, 300, 300, 100)
+fill("steelblue");
+circle(560, 100, 100);
+circle(980, 100, 100);
+line(720, 300, 820, 300);
+fill("gold")
+rect(375, 5, 300, 50);
+rect(855, 5, 300, 50);
+
+}
